287d75db8008e2eb394598c681d759f849cad4e6
vutran <tranvu221097@gmail.com>
2023-09-13 00:30:00 +0700

    change url

--- src/api/apiKey.js
+++ src/api/apiKey.js
@@ -1,7 +1,7 @@
 // API KEY AND IMAGE URL
 
 const API = {
-  MOVIE_URL: "http://localhost:8000",
+  MOVIE_URL: "https://movie-server-v7gy.onrender.com",
   IMAGE_URL: "https://image.tmdb.org/t/p/original/",
   API_KEY: "8qlOkxz4wq",
 };
